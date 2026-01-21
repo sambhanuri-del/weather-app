d1b3ad7e11c05b7d5e312eeb73f34d03a6cf9e21
Sam4705 <sambhanuri@gmail.com>
2026-01-21 10:53:41 +0530

    debug api key

--- weather.py
+++ weather.py
@@ -4,21 +4,28 @@ import os
 
 st.set_page_config(page_title="Weather App", page_icon="🌤️")
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 st.title("🌦️ Weather App")
 city = st.text_input("Enter city name")
 
 if st.button("Get Weather"):
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": city.strip(),
-        "appid": API_KEY,
-        "units": "metric"
-    }
+    if not city:
+        st.warning("Please enter a city name")
+    else:
+        url = "http://api.weatherapi.com/v1/current.json"
+        params = {
+            "key": API_KEY,
+            "q": city
+        }
 
-    response = requests.get(url, params=params)
+        response = requests.get(url, params=params)
+        data = response.json()
 
-    st.write("STATUS CODE:", response.status_code)
-    st.write("RAW RESPONSE:")
-    st.json(response.json())
+        if "error" in data:
+            st.error(data["error"]["message"])
+        else:
+            st.success(f"Weather in {data['location']['name']}")
+            st.write(f"🌡️ Temperature: {data['current']['temp_c']} °C")
+            st.write(f"☁️ Condition: {data['current']['condition']['text']}")
+            st.write(f"💨 Wind Speed: {data['current']['wind_kph']} kph")
